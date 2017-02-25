e6e5cba4a8c7cc45a341895100b74834fd7fd473
limte <taeu93@gmail.com>
2017-02-25 16:27:44 +0800

    button

--- LevelDesigner/LevelDesigner/LevelSelectViewController.swift
+++ LevelDesigner/LevelDesigner/LevelSelectViewController.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class LevelSelectViewController: UIViewController {
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true);
+        navigationController?.navigationBar.isHidden = true // for navigation bar hide
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
